da36d717f4a3967335e3a09ea9e6291d12879760
mafe <mfaj2697@hotmail.com>
2020-11-22 22:59:13 -0600

    Bootstrap update

--- JS/MyBootstrap.js
+++ JS/MyBootstrap.js
@@ -2,7 +2,7 @@
    /* Para que el video de youtube dentro del modal se cpause al cerrar el modal */ 
 
     /*<script>*/
-      $(document).ready(function(){
+    /*  $(document).ready(function(){
         $('#modalYT').on('hidden.bs.modal', function() {
           var $this = $(this).find('iframe'),
             tempSrc = $this.attr('src');
@@ -12,15 +12,6 @@
       });
     /*</script>*/
 
-    $(document).ready(function(){
-      $('.modal').each(function(){
-              var src = $(this).find('iframe').attr('src');
-  
-          $(this).on('click', function(){
-  
-              $(this).find('iframe').attr('src', '');
-              $(this).find('iframe').attr('src', src);
-  
-          });
-      });
-  });
+    $('#myModalPrev').on('hidden.bs.modal', function () {
+      $('#myModalPrev .modal-body').empty();
+   });
